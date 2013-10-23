3f5f06605ba8fa87d9a31d25ac6649d51da288ed
jan van der taelen <janvandertaelen@gmail.com>
2013-10-23 12:35:24 +0200

    begin

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -7,21 +7,21 @@
         <div class="main row">
             <ul id="og-grid" class="og-grid">
                 <li>
-                    <h5>Schilde</h5>
+                    <p class="winkels">Schilde</p>
                     <a href="http://cargocollective.com/jaimemartinez/" data-largesrc="/assets/images/1.png" data-title="Schilde" data-description="Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.">
                         <img src="/assets/images/thumbs/1.jpg" alt="img01"/>
                     </a>
                 </li>
                 
                 <li>
-                    <h5>Olen</h5>
+                    <p class="winkels">Olen</p>
                     <a href="http://cargocollective.com/jaimemartinez/" data-largesrc="/assets/images/2.png" data-title="Olen" data-description="Komatsuna prairie turnip wattle seed artichoke mustard horseradish taro rutabaga ricebean carrot black-eyed pea turnip greens beetroot yarrow watercress kombu.">
                         <img src="/assets/images/thumbs/2.png" alt="img02"/>
                     </a>
                 </li>
                 
                 <li>
-                    <h5>Deurne</h5>
+                    <p class="winkels">Deurne</p>
                     <a href="http://cargocollective.com/jaimemartinez/" data-largesrc="/assets/images/3.png" data-title="Deurne" data-description="Komatsuna prairie turnip wattle seed artichoke mustard horseradish taro rutabaga ricebean carrot black-eyed pea turnip greens beetroot yarrow watercress kombu.">
                         <img src="/assets/images/thumbs/3.png" alt="img02"/>
                     </a>
@@ -29,10 +29,15 @@
             </ul>
         </div>
     </div>
-    <div class="partners">
-        <div class="textvestigingen">
-            <h4>Partners</h4>
+    <div class="thule">
+        <div class="row">
+            <div class="col-md-4 ">
+                <img src="/assets/images/Thule.jpg"  />
+            </div>
         </div>
+    </div>
+    <div class="thuledescription">
+    
     </div>
 @stop
 
