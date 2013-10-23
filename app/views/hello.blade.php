d93faef3ef48ff250a537addd7aa936d3c91f0ad
Jan Van der Taelen <janvandertaelen@gmail.com>
2013-10-23 12:37:36 +0200

    Revert "begin"
    This reverts commit 3f5f06605ba8fa87d9a31d25ac6649d51da288ed.

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -7,21 +7,21 @@
         <div class="main row">
             <ul id="og-grid" class="og-grid">
                 <li>
-                    <p class="winkels">Schilde</p>
+                    <h5>Schilde</h5>
                     <a href="http://cargocollective.com/jaimemartinez/" data-largesrc="/assets/images/1.png" data-title="Schilde" data-description="Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.">
                         <img src="/assets/images/thumbs/1.jpg" alt="img01"/>
                     </a>
                 </li>
                 
                 <li>
-                    <p class="winkels">Olen</p>
+                    <h5>Olen</h5>
                     <a href="http://cargocollective.com/jaimemartinez/" data-largesrc="/assets/images/2.png" data-title="Olen" data-description="Komatsuna prairie turnip wattle seed artichoke mustard horseradish taro rutabaga ricebean carrot black-eyed pea turnip greens beetroot yarrow watercress kombu.">
                         <img src="/assets/images/thumbs/2.png" alt="img02"/>
                     </a>
                 </li>
                 
                 <li>
-                    <p class="winkels">Deurne</p>
+                    <h5>Deurne</h5>
                     <a href="http://cargocollective.com/jaimemartinez/" data-largesrc="/assets/images/3.png" data-title="Deurne" data-description="Komatsuna prairie turnip wattle seed artichoke mustard horseradish taro rutabaga ricebean carrot black-eyed pea turnip greens beetroot yarrow watercress kombu.">
                         <img src="/assets/images/thumbs/3.png" alt="img02"/>
                     </a>
@@ -29,15 +29,10 @@
             </ul>
         </div>
     </div>
-    <div class="thule">
-        <div class="row">
-            <div class="col-md-4 ">
-                <img src="/assets/images/Thule.jpg"  />
-            </div>
+    <div class="partners">
+        <div class="textvestigingen">
+            <h4>Partners</h4>
         </div>
-    </div>
-    <div class="thuledescription">
-    
     </div>
 @stop
 
--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -26,11 +26,49 @@
 <nav class="navbar navbar-default" role="navigation">
   <!-- Brand and toggle get grouped for better mobile display -->
   <div class="navbar-header">
-    <a class="navbar-brand" href="#"><img class="logo" src="/assets/images/logo.jpg" /></a>
+    <a class="navbar-brand" href="#">De Keyser Autoparts</a>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
   <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu1 <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li><a href="#">Separated link</a></li>
+          <li><a href="#">One more separated link</a></li>
+        </ul>
+      </li>
+    </ul>
+
+     <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu2 <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li><a href="#">Separated link</a></li>
+          <li><a href="#">One more separated link</a></li>
+        </ul>
+      </li>
+    </ul>
+
+     <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu3 <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li><a href="#">Separated link</a></li>
+          <li><a href="#">One more separated link</a></li>
+        </ul>
+      </li>
+    </ul>
    
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#">Particulier</a></li>
@@ -40,107 +78,7 @@
   </div><!-- /.navbar-collapse -->
 </nav>
 </div>
-<div class="row menubar">
-  <div class="span8 pull-right">
-  <ul class="menuTemplate1 decor1_1" license="mylicense">
-    <li><a href="#CSS-Menu">Auto onderdelen</a></li>
-    <li class="separator"></li>
-    <li><a href="#Horizontal-Menus" class="arrow">Reparaties</a>
-        <div class="drop decor1_2" style="width: 640px;">
-            <div class='left'>
-                <b>CSS Menu</b>
-                <div>
-                    <a href="#">Html 5 menu</a><br />
-                    <a href="#">Web menu CSS</a><br />
-                    <a href="#">Css menus</a><br />
-                    <a href="#">Nav element</a><br />
-                    <a href="#">SEO menu</a>
-                </div>
-            </div>
-            <div class='left'>
-                <b>Menu Templates</b>
-                <div>
-                    <a href="#">Menu template</a><br />
-                    <a href="#">Menu skin</a><br />
-                    <a href="#">Web menu styles</a><br />
-                    <a href="#">Best css menus</a>
-                </div>
-            </div>
-            <div class='left'>
-                <b>SEO Friendly</b>
-                <div>
-                    <a href="#">Search engine friendly</a><br />
-                    <a href="#">Best css menu</a><br />
-                    <a href="#">Most popular menus</a><br />
-                    <a href="#">Best web menu designs</a><br />
-                    <a href="#">E-Commerce sites</a><br />
-                    <a href="#">CSS Templates</a><br />
-                    <a href="#">Menu Design Templates</a><br />
-                    <a href="#">Clean layout & alignment</a><br />
-                    <a href="#">Favorite menus</a>
-                </div>
-            </div>
-        </div>
-    </li>
-    <li class="separator"></li>
-    <li><a href="#CSS">CSS</a></li>
-    <li class="separator"></li>
-    <li><a href="#Horizontal-Menu-CSS">Horizontal Menu CSS</a></li>
-    <li class="separator"></li>
-    <li><a href="#Web-Menu" class="arrow">Horizontal Web Menu</a>
-        <div class="drop decor1_2 dropToLeft2" style="width: 500px;">
-            <div class='left'>
-                <b>SHOPPING TOOLS</b>
-                <div>
-                    <a href="#">Build your script</a><br />
-                    <a href="#">Review current offers</a><br />
-                    <a href="#">See All ...</a>
-                </div>
-                <b>RSS FEED</b>
-                <div>
-                    <a href="#">Find a location</a><br />
-                    <a href="#">Request a test drive</a><br />
-                    <a href="#">See All ...</a>
-                </div>
-            </div>
-            <div class='left'>
-                <b>BE CREATIVE</b>
-                <div>
-                    <a href="#">Build your script</a><br />
-                    <a href="#">Review current offers</a><br />
-                    <a href="#">See All ...</a>
-                </div>
-            </div>
-            <div class='left' style="text-align:right; width:160px;">
-                <img src="../img/demo/hd7.png" style="width:128px; height:128px;" /><br />
-                Contact Us
-            </div>
-                
-        </div>
-    </li>
-    <li class="separator"></li>
-    <li><a href="#Web-Menus" class="arrow">Web Menus</a>
-        <div class="drop decor1_2 dropToLeft" style="width: 460px;">
-            <img src="../img/demo/css-menu-t3.jpg" style="float:left; width:98px;
-                height:180px;margin:10px 80px 10px 40px;" />
-            <div class='left'>
-                <b>PURE CSS MENU</b>
-                <div>
-                    <a href="#">Build your script</a><br />
-                    <a href="#">Review current offers</a><br />
-                    <a href="#">See All ...</a>
-                </div>
-                <b>POPOUT MENU</b>
-                <div>
-                    <a href="#">Find a location</a><br />
-                    <a href="#">Request a test drive</a><br />
-                    <a href="#">See All ...</a>
-                </div>
-            </div>
-        </div>
-    </li>
-</ul>
-</div>
+
 </div>
 
 	
